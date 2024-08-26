1b1f5c6b61219d2ae909cb46106f20ebbe352a7f
ras-rap <120177313+ras-rap@users.noreply.github.com>
2024-08-26 14:21:55 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Room 1":return img`
+e e e e e e e e e e e e e e e e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b b 
+e b b b b b b b b b b b b b b b 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e b b b b b b b b b b b b b b e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image2":
+            case "Player":return img`
+. . . . . . 5 5 5 5 . . . . . . 
+. . . . . 5 f 5 5 f 5 . . . . . 
+. . . . . 5 5 5 5 5 5 . . . . . 
+. . . . . 5 f 5 5 f 5 . . . . . 
+. . . . . . 5 f f 5 . . . . . . 
+. . . . 7 7 7 7 7 7 7 7 . . . . 
+. . . 7 7 7 7 7 7 7 7 7 7 . . . 
+. . . 7 7 7 7 7 7 7 7 7 7 . . . 
+. . . 7 7 7 7 7 7 7 7 7 7 . . . 
+. . . 5 5 7 7 7 7 7 7 5 5 . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . 7 7 . . 7 7 . . . . . 
+. . . . . 7 7 . . 7 7 . . . . . 
+. . . . . 7 7 . . 7 7 . . . . . 
+. . . . . e e e . e e e . . . . 
+. . . . . e e e . e e e . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
